807df85320f610ca6abb3b847c6781d67af82ae4
Andreas Remdt <me@andreasremdt.com>
2025-12-31 15:06:49 +0100

    feat: add correct sitemap entries

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,44 +1,15 @@
 import type { MetadataRoute } from 'next'
+import { getAllPages } from '@/lib/fetchers'
 
 const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
-    },
-    {
-      url: `${baseUrl}/news`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/clubs`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
-    {
-      url: `${baseUrl}/kontakt`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.5,
-    },
-    {
-      url: `${baseUrl}/impressum`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.3,
-    },
-    {
-      url: `${baseUrl}/datenschutz`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.3,
-    },
-  ]
+  const pages = await getAllPages()
+
+  return pages.map((page) => ({
+    url: `${baseUrl}/${page.slug}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: page.slug === 'home' ? 1 : 0.5,
+  }))
 }
